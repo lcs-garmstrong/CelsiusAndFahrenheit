0f09a158265022a06d56a2bc5776b0d14c09ec93
Graeme Armstrong <garmstrong@lcs.on.ca>
2023-01-17 12:27:03 -0500

    Initial Commit

---
+++ CelsiusAndFahrenheit/CelsiusAndFahrenheitApp.swift
@@ -0,0 +1,17 @@
+//
+//  CelsiusAndFahrenheitApp.swift
+//  CelsiusAndFahrenheit
+//
+//  Created by Graeme Armstrong on 2023-01-17.
+//
+
+import SwiftUI
+
+@main
+struct CelsiusAndFahrenheitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
